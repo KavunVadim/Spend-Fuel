8d7e738082bdcc18ef7031330bbe56a61411bdd2
unknown <kavunv@gmail.com>
2020-04-17 20:48:59 +0300

    add components

---
+++ src/Components/ChoiceMarkAuto/ChoiceMarkAuto.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './ChoiceMarkAuto.styles';
+
+const ChoiceMarkAuto = (props) => (
+  <div className="ChoiceMarkAutoWrapper">
+    Test content
+  </div>
+);
+
+ChoiceMarkAuto.propTypes = {
+  // bla: PropTypes.string,
+};
+
+ChoiceMarkAuto.defaultProps = {
+  // bla: 'test',
+};
+
+export default ChoiceMarkAuto;
