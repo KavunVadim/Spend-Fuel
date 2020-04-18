1ebdcac74cf836d5db73ef2a66749a2a513aa20f
unknown <kavunv@gmail.com>
2020-04-18 17:15:33 +0300

    Change folder. Because project did not correct setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ChoiceMarkAuto from "./Components/ChoiceMarkAuto/ChoiceMarkAuto";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import ControlledExpansionPanels from "./Components/ControlledExpansionPanels/ControlledExpansionPanels";
 
 function App() {
-  return <ChoiceMarkAuto />;
+  return <ControlledExpansionPanels />;
 }
 
 export default App;
